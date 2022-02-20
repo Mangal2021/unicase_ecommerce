152c9237827ab3f204d744f333f130f5d420c2de
ritesh <rkchy72@gmail.com>
2022-02-20 20:16:26 +0545

    profile updated

--- unicase_ecom/route.py
+++ unicase_ecom/route.py
@@ -1,3 +1,4 @@
 routes = [
 	    {'from_route':'/Member/index/<docname>', 'to_route':'Member/index'},
+        {'from_route':'/MemberProfile/index/<docname>', 'to_route':'MemberProfile/index'},
     ]
